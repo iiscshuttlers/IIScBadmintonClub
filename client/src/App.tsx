1517458823f1cd011e2e9a4da2c1e30c39e52f18
Raja <rajajanmejaya@gmail.com>
2026-05-01 13:25:10 +0530

    Farewell Match

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,20 @@
-import { Toaster } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import NotFound from "@/pages/NotFound";
-import { Route, Switch, Router } from "wouter";
-import ErrorBoundary from "./components/ErrorBoundary";
-import { ThemeProvider } from "./contexts/ThemeContext";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Facilities from "./pages/Facilities";
-import Events from "./pages/Events";
+import { Toaster } from '@/components/ui/sonner';
+import { TooltipProvider } from '@/components/ui/tooltip';
+import NotFound from '@/pages/NotFound';
+import { Route, Switch, Router } from 'wouter';
+import ErrorBoundary from './components/ErrorBoundary';
+import { ThemeProvider } from './contexts/ThemeContext';
+import Home from './pages/Home';
+import About from './pages/About';
+import Facilities from './pages/Facilities';
+import Events from './pages/Events';
 import Announcements from './pages/Announcements';
-import Gallery from "./pages/Gallery";
-import Contact from "./pages/Contact";
-import Navigation from "./components/Navigation";
-import Footer from "./components/Footer";
-import TournamentDetail from "./pages/TournamentDetail";
+import FarewellMatchPage from './pages/FarewellMatchPage';
+import Gallery from './pages/Gallery';
+import Contact from './pages/Contact';
+import Navigation from './components/Navigation';
+import Footer from './components/Footer';
+import TournamentDetail from './pages/TournamentDetail';
 
 function AppRoutes() {
   return (
@@ -24,6 +25,7 @@ function AppRoutes() {
       <Route path="/events" component={Events} />
       <Route path="/events/:slug" component={TournamentDetail} />
       <Route path="/announcements" component={Announcements} />
+      <Route path="/events/farewell-match" element={<FarewellMatchPage />} />
       <Route path="/gallery" component={Gallery} />
       <Route path="/contact" component={Contact} />
       <Route path="/404" component={NotFound} />
